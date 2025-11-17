9d3eed8ca0d396cfb1be2963f08fc02be844e55b
William McIntosh <46854008+williammcintosh@users.noreply.github.com>
2025-11-18 00:31:12 +1300

    reset beginning board size

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ const gameState = {
 };
 gameState.score = gameState.score || 0;
 
-const base = { stones: 5, board: 5, time: 40 };
+const base = { stones: 5, board: 4, time: 40 };
 
 for (let i = 1; i <= 50; i++) {
   const boardSize = base.board + Math.floor((i - 1) / 5);
